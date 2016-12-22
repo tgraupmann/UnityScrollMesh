41a91fc3af5bff4a2a56be38e1a60276c5deb0ea
Tim Graupmann <tgraupmann@gmail.com>
2016-12-22 11:33:39 -0800

    Adjust force if contact is made or lost

--- Assets/ScrollMesh/Scripts/Player.cs
+++ Assets/ScrollMesh/Scripts/Player.cs
@@ -4,19 +4,17 @@ namespace ScrollMesh
 {
     public class Player : MonoBehaviour
     {
-        public float _mSpeed = 10f;
-        public float _mMoveForce = 2f;
+        public float _mMoveForce = 100f;
         public float _mJumpForce = 5f;
-        public float _mDampening = 10f;
         public TerrainData _mTerrainData = null;
         public Rigidbody _mRigidBody = null;
         public GameObject _mGraphics = null;
 
         private void Start()
         {
-            Vector3 pos = transform.position;
+            Vector3 pos = _mRigidBody.transform.position;
             pos.y = _mTerrainData.GetHeight(pos.x) + 5;
-            transform.position = pos;
+            _mRigidBody.transform.position = pos;
         }
 
         // Update is called once per frame
@@ -25,7 +23,7 @@ namespace ScrollMesh
             Vector3 pos = transform.position;
             if (Input.GetKey(KeyCode.A))
             {
-                Vector3 force = ((-_mSpeed * Vector3.right) + (_mMoveForce * Vector3.up)) * Time.deltaTime;
+                Vector3 force = -_mMoveForce * Vector3.right * Time.deltaTime;
                 _mRigidBody.AddForce(force, ForceMode.VelocityChange);
 
                 _mGraphics.transform.rotation = Quaternion.Lerp(
@@ -36,7 +34,7 @@ namespace ScrollMesh
 
             if (Input.GetKey(KeyCode.D))
             {
-                Vector3 force = ((_mSpeed * Vector3.right) + (_mMoveForce * Vector3.up)) * Time.deltaTime;
+                Vector3 force = _mMoveForce * Vector3.right * Time.deltaTime;
                 _mRigidBody.AddForce(force, ForceMode.VelocityChange);
 
                 _mGraphics.transform.rotation = Quaternion.Lerp(
@@ -49,6 +47,8 @@ namespace ScrollMesh
             {
                 _mRigidBody.AddForce(_mJumpForce * Vector3.up, ForceMode.VelocityChange);
             }
+
+            transform.position = _mRigidBody.transform.position;
         }
     }
 }
---
+++ Assets/ScrollMesh/Scripts/PlayerSphere.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ScrollMesh
+{
+    public class PlayerSphere : MonoBehaviour
+    {
+        public Player _mPlayer = null;
+
+        void OnCollisionEnter(Collision collision)
+        {
+            _mPlayer._mMoveForce = 100f;
+        }
+
+        void OnCollisionExit(Collision collision)
+        {
+            _mPlayer._mMoveForce = 1f;
+        }
+    }
+}
